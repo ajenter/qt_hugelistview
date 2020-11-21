54f4f85208168d4731dfb81843e07496df9bbe19
Александр Шахраманян <alex.jenter@gmail.com>
2020-11-21 20:18:36 +0700

    switch to TableView

--- src/customdelegate.cpp
+++ src/customdelegate.cpp
@@ -6,13 +6,14 @@
 
 CustomDelegate::CustomDelegate(QObject* parent)
     : QItemDelegate(parent),
-      _cachedItemHeights(10000) // make cache large enough to hold all items
+      _cachedItemHeights(1000) // make cache large enough to hold all items
 {
 }
 
 void CustomDelegate::paint(QPainter* painter, const QStyleOptionViewItem& opt, const QModelIndex& index) const
 {
 	const auto rect = opt.rect.adjusted(0, 0, -2, -1);
+	painter->setPen(opt.state & QStyle::State_HasFocus ? Qt::red : Qt::black);
 	painter->drawRect(rect);
 
 	QString text = index.model()->data(index, Qt::DisplayRole).toString();
@@ -21,24 +22,23 @@ void CustomDelegate::paint(QPainter* painter, const QStyleOptionViewItem& opt, c
 
 QSize CustomDelegate::sizeHint(const QStyleOptionViewItem&, const QModelIndex& index) const
 {
-	qDebug() << "size hint requested for index #" << index.row();
+	//qDebug() << "size hint requested for index #" << index.row();
 
 	int* cachedItemHeight;
 	if (cachedItemHeight = _cachedItemHeights[index.row()])
 	{
-		qDebug() << "  took value from cache";
+		//qDebug() << "  took value from cache";
 		return QSize(0, *cachedItemHeight);
 	}
 
-	// sleep to emulate text layouting taking place
-	QThread::msleep(1);
-
 	// make height depend somehow on index to emulate items with various heights
-	const int itemHeight = 20 * (1 + index.row() % 10);
+	int itemHeight = 1;
+	//for (int i = 0; i < 10000; ++i)
+	itemHeight = int((itemHeight % 5) + 20 * (1 + (rand() % 100) / 10.0));
 	qDebug() << "  calculating item height for index #" << index.row() << ", itemHeight = " << itemHeight;
 
 	// add to cached
 	_cachedItemHeights.insert(index.row(), new int(itemHeight));
 
-	return QSize(0, itemHeight);
+	return QSize(100, itemHeight);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 
 #include <QAbstractListModel>
 #include <QApplication>
-#include <QListView>
+#include <QHeaderView>
+#include <QItemSelection>
+#include <QScrollBar>
+#include <QTableView>
 
 class Model : public QAbstractListModel
 {
@@ -13,7 +16,7 @@ protected:
 	virtual int rowCount(const QModelIndex& parent) const override
 	{
 		if (parent.isValid()) return 0;
-		return 5000;
+		return 100000;
 	}
 
 	virtual QVariant data(const QModelIndex& index, int role) const
@@ -29,18 +32,57 @@ protected:
 	}
 };
 
+void updateRowHeights(QTableView& table)
+{
+	static QItemSelection sel;
+
+	const QRect viewportRect = table.viewport()->rect();
+
+	QModelIndex topRowIndex = table.indexAt(QPoint(viewportRect.x() + 5, viewportRect.y() + 5));
+	QModelIndex bottomRowIndex = table.indexAt(QPoint(viewportRect.x() + 5, viewportRect.y() + viewportRect.height() - 5));
+	//qDebug() << "top row: " << topRowIndex.row() << ", bottom row: " << bottomRowIndex.row();
+
+	for (auto i = topRowIndex.row() - 1; i < bottomRowIndex.row() + 1; ++i)
+	{
+		auto index = table.model()->index(i, 0);
+		if (!sel.contains(index))
+		{
+			table.resizeRowToContents(i);
+			sel.select(index, index);
+			//qDebug() << "Marked row #" << i << " as resized";
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
 
 	Model model;
 
-	QListView listView;
-	listView.setLayoutMode(QListView::Batched);
-	listView.setBatchSize(10);
-	listView.setItemDelegate(new CustomDelegate(&listView));
-	listView.setModel(&model);
-	listView.show();
+	QTableView table;
+	//listWidget.setLayoutMode(QListView::Batched);
+	//listWidget.setBatchSize(10);
+	table.resize(600, 800);
+	table.setSelectionBehavior(QAbstractItemView::SelectRows);
+	table.horizontalHeader()->setStretchLastSection(true);
+	table.setItemDelegateForColumn(0, new CustomDelegate(&table));
+	table.horizontalHeader()->hide();
+	table.verticalHeader()->hide();
+	//table.verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+	table.setModel(&model);
+	table.setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+	table.show();
+
+	updateRowHeights(table);
+
+	//for (int i = 0; i < 100; ++i)
+	//table.resizeRowToContents(i);
+
+	table.connect(table.verticalScrollBar(), &QScrollBar::valueChanged, [&table](int newValue) {
+		//qDebug() << "valueChanged: " << newValue;
+		updateRowHeights(table);
+	});
 
 	// NOTE: it takes several seconds this the list is loaded!!!
 	// debug logs show that sizeHint for ALL items is requested upon startup
